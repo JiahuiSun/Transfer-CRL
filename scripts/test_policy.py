0ace8f615b5f98270c4291c3335508505bcb5790
JiahuiSun <jhsun1997@sjtu.edu.cn>
2022-04-28 08:36:05 +0000

    test policy

--- scripts/test_policy.py
+++ scripts/test_policy.py
@@ -1,13 +1,11 @@
 #!/usr/bin/env python
-
 import time
 import numpy as np
 from safe_rl.utils.load_utils import load_policy
 from safe_rl.utils.logx import EpochLogger
 
 
-
-def run_policy(env, get_action, max_ep_len=None, num_episodes=100, render=True):
+def run_policy(env, get_action, max_ep_len=None, num_episodes=100, render=True, cost_lim=20):
 
     assert env is not None, \
         "Environment not found!\n\n It looks like the environment wasn't saved, " + \
@@ -15,6 +13,7 @@ def run_policy(env, get_action, max_ep_len=None, num_episodes=100, render=True):
 
     logger = EpochLogger()
     o, r, d, ep_ret, ep_cost, ep_len, n = env.reset(), 0, False, 0, 0, 0, 0
+    o = np.append(o, cost_lim)
     while n < num_episodes:
         if render:
             env.render()
@@ -26,11 +25,13 @@ def run_policy(env, get_action, max_ep_len=None, num_episodes=100, render=True):
         ep_ret += r
         ep_cost += info.get('cost', 0)
         ep_len += 1
+        o = np.append(o, cost_lim-ep_cost)
 
         if d or (ep_len == max_ep_len):
             logger.store(EpRet=ep_ret, EpCost=ep_cost, EpLen=ep_len)
             print('Episode %d \t EpRet %.3f \t EpCost %.3f \t EpLen %d'%(n, ep_ret, ep_cost, ep_len))
             o, r, d, ep_ret, ep_cost, ep_len = env.reset(), 0, False, 0, 0, 0
+            o = np.append(o, cost_lim)
             n += 1
 
     logger.log_tabular('EpRet', with_min_and_max=True)
@@ -48,8 +49,9 @@ if __name__ == '__main__':
     parser.add_argument('--norender', '-nr', action='store_true')
     parser.add_argument('--itr', '-i', type=int, default=-1)
     parser.add_argument('--deterministic', '-d', action='store_true')
+    parser.add_argument('--cost_lim', type=float, default=20)
     args = parser.parse_args()
     env, get_action, sess = load_policy(args.fpath,
                                         args.itr if args.itr >=0 else 'last',
                                         args.deterministic)
-    run_policy(env, get_action, args.len, args.episodes, not(args.norender))
+    run_policy(env, get_action, args.len, args.episodes, not(args.norender), cost_lim=args.cost_lim)
